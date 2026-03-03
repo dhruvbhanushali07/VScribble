b02f305345cae9c119bfd1246f405e5396a534f5
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2026-03-03 09:39:49 +0530

    Add routing for Lobby and Room components in main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Lobby from "./components/Lobby.tsx";
+import Room from "./components/Room.tsx";
 
 createRoot(document.getElementById("root")!).render(
 	<BrowserRouter>
 		<StrictMode>
 			<Routes>
 				<Route path="/" element={<App />}>
-					<Route path="/room/:slug" element />
+					<Route index element={<Lobby/>}/>
+					<Route path="/room/:roomId" element={<Room/>}/>
 				</Route>
 			</Routes>
 		</StrictMode>
