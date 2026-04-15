7e283ff3e6d9a353eb62989746c0f4e7dbb8d29f
Dhruv Bhanushali <dhruvbhanushali0007@gmail.com>
2026-04-15 11:09:15 +0530

    Update SocketContext to use environment variable for backend URL

--- frontend/src/context/SocketContext.ts
+++ frontend/src/context/SocketContext.ts
@@ -2,7 +2,7 @@ import { createContext } from "react";
 import { io, Socket } from "socket.io-client";
 
 type MySocketType = Socket | null;
-
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
 export const socket: Socket = io("http://localhost:5000");
 
 
